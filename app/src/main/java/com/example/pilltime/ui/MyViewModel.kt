490fcc963018738ad50d965633ee395201c9b786
pureum <lpm0831@naver.com>
2023-11-07 13:17:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pilltime/ui/MyViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pilltime.ui
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * 2023-11-07
+ * pureum
+ */
+class MyViewModel: ViewModel() {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pill Time"
+include(":app")
+include(":data")
